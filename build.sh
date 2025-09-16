91c46db915cf36a538a177f984cc2c929323fee7
Alistair Smith <hi@alistair.sh>
2025-09-16 16:12:24 -0700

    update local build script

--- build.sh
+++ build.sh
@@ -1,9 +1,26 @@
 yarn build react/index,react-dom/index,react-server-dom-bun
-cd build/oss-stable/react-server-dom-bun
-bun pm pack --filename tarball.tgz
-$TARBALL=$(pwd)/tarball.tgz
-echo $TARBALL
+
+# pack react
+cd build/oss-stable/react
+bun pm pack --filename tarball-react.tgz
+TARBALL_REACT=$(pwd)/tarball-react.tgz
+echo $TARBALL_REACT
+
+# pack react-dom
+cd ../react-dom
+bun pm pack --filename tarball-react-dom.tgz
+TARBALL_REACT_DOM=$(pwd)/tarball-react-dom.tgz
+echo $TARBALL_REACT_DOM
+
+# pack react-server-dom-bun
+cd ../react-server-dom-bun
+bun pm pack --filename tarball-react-server-dom-bun.tgz
+TARBALL_REACT_SERVER_DOM_BUN=$(pwd)/tarball-react-server-dom-bun.tgz
+echo $TARBALL_REACT_SERVER_DOM_BUN
+
+# update bun
 cd ~/code/bun/default/packages/bun-framework-react
-bun remove react-server-dom-bun
-bun add react-server-dom-bun@$TARBALL
+bun remove react react-dom react-server-dom-bun
+bun add react@$TARBALL_REACT react-dom@$TARBALL_REACT_DOM react-server-dom-bun@$TARBALL_REACT_SERVER_DOM_BUN
+
 cd ~/code/react
--- packages/shared/ReactVersion.js
+++ packages/shared/ReactVersion.js
@@ -1,1 +1,1 @@
-export default '19.2.0-canary-7dd00866-20250916';
+export default '19.2.0-canary-8c3655ad-20250916';
