6e44123ea613a10601494fc58dcea4365c66dd45
Alistair Smith <hi@alistair.sh>
2025-09-16 16:22:20 -0700

    build scheduler

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-yarn build react/index,react-dom/index,react-server-dom-bun
+# Build command (uncomment to build):
+yarn build react/index,react-dom/index,react-server-dom-bun,scheduler --type=NODE
 
 # pack react
 cd build/oss-stable/react
@@ -18,9 +19,14 @@ bun pm pack --filename tarball-react-server-dom-bun.tgz
 TARBALL_REACT_SERVER_DOM_BUN=$(pwd)/tarball-react-server-dom-bun.tgz
 echo $TARBALL_REACT_SERVER_DOM_BUN
 
-# update bun
-cd ~/code/bun/default/packages/bun-framework-react
-bun remove react react-dom react-server-dom-bun
-bun add react@$TARBALL_REACT react-dom@$TARBALL_REACT_DOM react-server-dom-bun@$TARBALL_REACT_SERVER_DOM_BUN
+# pack scheduler
+cd ../scheduler
+bun pm pack --filename tarball-scheduler.tgz
+TARBALL_SCHEDULER=$(pwd)/tarball-scheduler.tgz
+echo $TARBALL_SCHEDULER
 
-cd ~/code/react
+cd /Users/ali/code/bun/default/packages/bun-framework-react
+bun remove react react-dom react-server-dom-bun scheduler
+
+echo install command is:
+echo bun add react@$TARBALL_REACT react-dom@$TARBALL_REACT_DOM react-server-dom-bun@$TARBALL_REACT_SERVER_DOM_BUN scheduler@$TARBALL_SCHEDULER
--- packages/shared/ReactVersion.js
+++ packages/shared/ReactVersion.js
@@ -1,1 +1,1 @@
-export default '19.2.0-canary-8c3655ad-20250916';
+export default '19.2.0-canary-91c46db9-20250916';
