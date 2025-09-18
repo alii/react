ad5b87ffe1d0201701a9cdca62446609fae131f7
Alistair Smith <hi@alistair.sh>
2025-09-18 13:52:38 -0700

    bun

--- packages/react-client/src/forks/ReactFlightClientConfig.dom-bun.js
+++ packages/react-client/src/forks/ReactFlightClientConfig.dom-bun.js
@@ -12,18 +12,6 @@ export const rendererPackageName = 'react-server-dom-bun';
 
 export * from 'react-client/src/ReactFlightClientStreamConfigWeb';
 export * from 'react-client/src/ReactClientConsoleConfigPlain';
+export * from 'react-server-dom-bun/src/client/ReactFlightClientConfigBundlerBun';
 export * from 'react-dom-bindings/src/shared/ReactFlightClientConfigDOM';
-
-export opaque type ModuleLoading = mixed;
-export opaque type ServerConsumerModuleMap = mixed;
-export opaque type ServerManifest = mixed;
-export opaque type ServerReferenceId = string;
-export opaque type ClientReferenceMetadata = mixed;
-export opaque type ClientReference<T> = mixed; // eslint-disable-line no-unused-vars
-export const resolveClientReference: any = null;
-export const resolveServerReference: any = null;
-export const preloadModule: any = null;
-export const requireModule: any = null;
-export const getModuleDebugInfo: any = null;
-export const prepareDestinationForModule: any = null;
 export const usedWithSSR = true;
--- packages/shared/ReactVersion.js
+++ packages/shared/ReactVersion.js
@@ -1,1 +1,1 @@
-export default '19.2.0-canary-4bca394c-20250917';
+export default '19.2.0-canary-a1340ce5-20250918';
